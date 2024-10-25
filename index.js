3407052c375ea40fb1070206f2f1c759e2ff21a1
Siuozl <104869004+Siuozl@users.noreply.github.com>
2024-10-25 19:21:00 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 let txt = document.getElementByID('text');
-    	function servers(){
-            txt.innerText = "Server was clicked.";
-        };
+function servers(){
+    txt.innerText = "Server was clicked.";
+};
