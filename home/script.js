f462f96ddf3be0d6056d199c054451df4a3c66f8
Ignitemare1680 <ImIgnitemare@outlook.com>
2025-02-16 15:28:42 -0800

    Update script.js

--- home/script.js
+++ home/script.js
@@ -1,8 +1,9 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const hamburger = document.querySelector('.navbar .hamburger');
+// Wait for the DOM to load before executing the script
+document.addEventListener("DOMContentLoaded", function() {
+    const hamburger = document.querySelector('.hamburger');
     const dropdown = document.querySelector('.navbar .dropdown');
-    
-    // Toggle dropdown visibility when hamburger is clicked
+
+    // When hamburger icon is clicked, toggle the dropdown menu
     hamburger.addEventListener('click', function() {
         dropdown.classList.toggle('active');
     });
