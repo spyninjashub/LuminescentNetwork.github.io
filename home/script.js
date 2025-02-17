bdf42af47e368804c0d420e661928611a8dff629
Orca <124471477+OrcaTheBeginner@users.noreply.github.com>
2025-02-17 12:11:34 -0500

    Hopefully Fix The Problem With The Bar

--- home/script.js
+++ home/script.js
@@ -8,3 +8,22 @@ document.addEventListener("DOMContentLoaded", function() {
         dropdown.classList.toggle('active');
     });
 });
+
+var bar = document.getElementById("nmBar");
+function hideBar () { if (
+        navigator.userAgent.includes("iPhone") ||
+        navigator.userAgent.includes("iPod") ||
+        navigator.userAgent.includes("iPad") ||
+        navigator.userAgent.includes("Android") ||
+        navigator.userAgent.includes("Windows Phone") ||
+        navigator.userAgent.includes("BlackBerry") ||
+        navigator.userAgent.includes("Windows NT") ||
+        navigator.userAgent.includes("Mobile") 
+                            ) 
+    {
+     bar.style.display = "none";
+     console.log(true);
+     console.log(navigator.userAgent);   
+    }
+                      };
+
